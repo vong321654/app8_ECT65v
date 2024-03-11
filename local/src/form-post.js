f550040bef431b7cfd9c7ee76fd59596715d6733
vong321654 <141673546+vong321654@users.noreply.github.com>
2024-03-11 13:17:04 +0000

    restApiLocalnServer

--- local/src/form-post.js
+++ local/src/form-post.js
@@ -24,7 +24,13 @@ export default function FormPost(){
         <div style={{margin:'30px'}}>
             <form ref={form} onSub={onSubmitForm}>
                 <div>ติดต่อเรา</div>
+                <input type="text" name="name" size="43" placeholder="ชื่อ" style={inputStyle}/><br/>
+                <input type="email" name="email" size="43" placeholder="อีเมล" style={inputStyle}/><br/>
+                <textarea name="message" col="40" rows="4" placeholder="ข้อความ"></textarea><br/>
+                <button>ตกลง</button>
             </form>
+            <br/>
+            <div dangerouslySetInnerHTML={{_html:postedData}}></div>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 const express=require('express')
 const app=express()
 const port=8000
+app.use(express.urlencoded({extended: ture}))
+app.use(express.json())
+app.use(express.static('public'))
 
-app.get('/',(request,response)=>{
-    response.send(`
-        <!doctype html>
-        <html>
-            <head>
-                <title>Express.js</title>
-            </head>
-            <body>
-                <h3>
-                    welcome to Express.js
-                </h3>
-                <b>Express.js Fast, unopinionated, minimalist<br>web framework for node.js</b>
-            </body>
-        </html>
-    `)
+app.post('/api/form-post', (request, response) => {
+    let name = request.body.name ||''
+    let email = request.body.email ||''
+    let msg = request.body.msg ||''
+
+    let test =`
+     <table border="1">
+        <caption>ข้อมูลที่ส่งขึ้นไป</caption>
+        <tr><td>ชื่อ:</td><td>${name}</td></tr>
+        <tr><td>อีเมล:</td><td>${email}</td></tr>
+        <tr><td>ข้อความ:</td><td>${msg}</td></tr>
+     </table>
+    `
+    
+    response.send(text)
 })
 app.listen(port, () => console.log('Server listing on port'+port))
